2929c9aba66298351e69bb0ae52265121ba1f3dc
Umer memon <117743102+umermemon4648@users.noreply.github.com>
2024-06-15 01:30:48 +0500

    page not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,13 +1,29 @@
 import React from "react";
+import pagenotfound from "../assets/404.png";
+import { Link } from "react-router-dom";
+import { Flex, Image, Button, Row, Col } from "antd";
 
-import { Typography, Space } from "antd";
-
-const { Text } = Typography;
 const NotFound = () => {
   return (
-    <Space direction="vertical">
-      <Text>home page</Text>
-    </Space>
+    <Col className="h-screen">
+      <Image
+        preview={false}
+        src={pagenotfound}
+        style={{
+          width: "40%",
+        }}
+        className="!h-264-[px] w-full object-contain my-4 mx-auto"
+      />
+      <Row className="w-full">
+        <Flex className="mx-auto">
+          <Link to={"/"}>
+            <Button className="primary-btn px-8 py-2 h-full w-full">
+              Back To Home
+            </Button>
+          </Link>
+        </Flex>
+      </Row>
+    </Col>
   );
 };
 
